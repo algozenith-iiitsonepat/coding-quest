78181a7bc5a9f6463ad22a6ae0d8d9669ba3f2a9
Dipankar <dipankaryadav1234@gmail.com>
2023-11-03 10:23:47 +0530

    3rd November

--- November 2023/3rd November/1441. Build an Array With Stack Operations.cpp
+++ November 2023/3rd November/1441. Build an Array With Stack Operations.cpp
@@ -11,14 +11,17 @@ class Solution
                 {
                     ans.push_back("Push");
                     j++;
+                 /* If the element from the input stream is equal to the element of target array then we push it to the stack      */
                 }
                 else
-                {
+                {   
+                    /* If the element from the input stream is not equal to the element of target array then we first push it to the stack and then remove it      */
                     ans.push_back("Push");
                     ans.push_back("Pop");
                 }
                 if (j == target.size())
                 {
+                    /* If we reach the end of the target array then we simply break out of the loop */
                     break;
                 }
             }
