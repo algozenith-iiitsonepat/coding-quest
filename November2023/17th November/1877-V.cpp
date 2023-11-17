c740e71f11766cd5670d2d7ad6ddc4a5dade48f2
vrishavgarg <gargvrishav@gmail.com>
2023-11-17 11:42:20 +0530

    day 17 by Vrishav

--- November2023/17th November/1877-V.cpp
+++ November2023/17th November/1877-V.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int minPairSum(vector<int>& nums) {
         int n=nums.size(),ans=0;
+        // First we will sort array as we want to add ith smallest element
+        // to ith largest element to minimize the maximum sum
         sort(nums.begin(),nums.end());
         for(int i=0; i<n/2; i++){
             ans=max(ans,nums[i]+nums[n-i-1]);
