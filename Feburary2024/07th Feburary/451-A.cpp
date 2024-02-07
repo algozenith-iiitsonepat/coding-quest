7d4bb3f1add61c07cc4d216c1259641454f683e0
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-02-07 20:16:19 +0530

    7th feb

---
+++ Feburary2024/07th Feburary/451-A.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        vector<pair<int, char>> v;
+        string ans = "";
+
+        for (auto ch: s) {
+            mp[ch]++;
+        }
+        for (auto i: mp) {
+            v.push_back({i.second, i.first});
+        }
+        sort(v.begin(), v.end(), greater<pair<int, char>>());
+        for (auto i: v) {
+            while (i.first--) ans+=i.second;
+        }
+        return ans;
+    }
+};
