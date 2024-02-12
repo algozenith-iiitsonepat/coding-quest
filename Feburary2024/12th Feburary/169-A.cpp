7ef7a15cdef327a3ffef5e03a89a3bd58800707c
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-02-12 17:51:14 +0530

    12th feb

---
+++ Feburary2024/12th Feburary/169-A.cpp
@@ -0,0 +1,12 @@
+class Solution {
+	public:
+		int majorityElement(vector<int>& nums) {
+			int n = nums.size();
+			map<int, int> mp;
+			for (int i = 0; i< n; i++) {
+				mp[nums[i]]++;
+				if (mp[nums[i]]> n/2) return nums[i];
+			}
+			return -1;
+		}
+};
